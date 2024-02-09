37c0d4917af913b8323514d71f71b744ae8565e0
Richard Xu <richard.xu@roadbotics.com>
2024-02-09 18:13:23 -0500

    Add failing it-returns-for-valid-game-2

--- src/advent_of_code/day2/day2_test.clj
+++ src/advent_of_code/day2/day2_test.clj
@@ -4,3 +4,6 @@
 
 (deftest it-returns-for-valid-game-1
   (is (= 1 (day2/calculate-sum-of-ids ["Game 1: 1 blue"]))))
+
+(deftest it-returns-for-valid-game-2
+  (is (= 2 (day2/calculate-sum-of-ids ["Game 1: 100 blue" "Game 2: 1 blue"]))))
