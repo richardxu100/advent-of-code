01837fb2dde092e230fe24ec3c09c944efaff94d
Richard Xu <richard.xu@roadbotics.com>
2024-02-08 22:27:58 -0500

    Make test pass

--- src/advent_of_code/day1/part2.clj
+++ src/advent_of_code/day1/part2.clj
@@ -1,6 +1,13 @@
 (ns advent-of-code.day1.part2)
 
+(defn convert-string-to-number
+  "docstring"
+  [string]
+  (- (int string) (int \0)))
+
 (defn calibration-total
   "docstring"
   [calibrations]
-  nil)
+  (let [value (first calibrations)]
+    (+ (* 10 (convert-string-to-number (first value)) ) (convert-string-to-number (last value)))))
+
