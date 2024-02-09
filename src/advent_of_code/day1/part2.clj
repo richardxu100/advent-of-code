ea275784f6cc61b16a79342c40917a5f615a0e55
Richard Xu <richard.xu@roadbotics.com>
2024-02-08 22:43:27 -0500

    Make test pass

--- src/advent_of_code/day1/part2.clj
+++ src/advent_of_code/day1/part2.clj
@@ -1,6 +1,23 @@
 (ns advent-of-code.day1.part2
   (:require [clojure.string :as str]))
 
+(defn parse-number
+  "docstring"
+  [string]
+  (case string
+    "zero" 0
+    "one" 1
+    "two" 2
+    "three" 3
+    "four" 4
+    "five" 5
+    "six" 6
+    "seven" 7
+    "eight" 8
+    "nine" 9
+    nil))
+; can keep the same parse-number, just formulate the number from right to left
+
 (defn left-most-number
   "docstring"
   [string]
@@ -14,16 +31,29 @@
   [string]
   (left-most-number (reverse string)))
 
+(defn left-most-number-with-string-parsing
+  "docstring"
+  [string]
+  (loop [remaining-string string
+         current-string ""]
+    (cond
+      (Character/isDigit (first remaining-string))
+      (- (int (first remaining-string)) (int \0))
+      (not (nil? (parse-number (str current-string (first remaining-string)))))
+      (parse-number (str current-string (first remaining-string)))
+      :else
+      (recur (rest remaining-string) (str current-string (first remaining-string))))))
+
+
 (defn calc-calibration-value
   "docstring"
   [line]
-  (+ (* 10 (left-most-number line) ) (right-most-number line)))
+  (+ (* 10 (left-most-number-with-string-parsing line) ) (right-most-number line)))
 
 (defn calibration-total
   "docstring"
   [calibrations]
   (apply + (map calc-calibration-value calibrations)) )
-
 
 (-> "./src/advent_of_code/calibration-values.txt"
     slurp
