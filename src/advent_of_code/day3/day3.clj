6d3aa1ecdde5312b39558f62e02aca2ae9784c54
Richard Xu <richard.xu@roadbotics.com>
2024-02-11 14:23:58 -0500

    Make test pass

--- src/advent_of_code/day3/day3.clj
+++ src/advent_of_code/day3/day3.clj
@@ -34,33 +34,36 @@
         down-neighbor [current-num-index (inc y)]]  ; add more tests, change to all-neighbors or something. tdd can be weird
     (or (and (is-in-graph? left-neighbor graph) (is-symbol? (get-graph-value graph left-neighbor)))
         (and (is-in-graph? right-neighbor graph) (is-symbol? (get-graph-value graph right-neighbor)))
-        (and (is-in-graph? down-neighbor graph) (is-symbol? (get-graph-value graph down-neighbor)))
-        )))
+        (and (is-in-graph? down-neighbor graph) (is-symbol? (get-graph-value graph down-neighbor))))))
+
+(defn find-valid-nums
+  "docstring"
+  [graph row y]
+  (loop [index 0
+         remaining row
+         current-num ""
+         current-num-index nil
+         valid-nums []]
+    (cond
+      (empty? remaining)
+      (if (empty? current-num)
+        (apply + valid-nums)
+        (if (is-valid-num current-num current-num-index y graph)
+          (let [all-valid-nums (conj valid-nums (Integer/parseInt current-num))]
+            (apply + all-valid-nums))
+          (apply + valid-nums)))
+      (is-digit? (first remaining))
+      (recur (inc index) (rest remaining) (str current-num (first remaining)) (if (nil? current-num-index)
+                                                                                index
+                                                                                current-num-index) valid-nums)
+      (not (empty? current-num))
+      (recur (inc index) (rest remaining) "" nil (if (is-valid-num current-num current-num-index y graph)
+                                                   (conj valid-nums (Integer/parseInt current-num))
+                                                   valid-nums))
+      :else
+      (recur (inc index) (rest remaining) current-num current-num-index valid-nums))))
 
 (defn sum-of-parts
   "docstring"
   [graph]
-  (let [row (first graph)]
-    (loop [index 0
-           remaining row
-           current-num ""
-           current-num-index nil
-           valid-nums []]
-      (cond
-        (empty? remaining)
-        (if (empty? current-num)
-          (apply + valid-nums)
-          (if (is-valid-num current-num current-num-index 0 graph)
-            (let [all-valid-nums (conj valid-nums (Integer/parseInt current-num))]
-              (apply + all-valid-nums))
-            (apply + valid-nums)))
-        (is-digit? (first remaining))
-        (recur (inc index) (rest remaining) (str current-num (first remaining)) (if (nil? current-num-index)
-                                                                                  index
-                                                                                  current-num-index) valid-nums)
-        (not (empty? current-num))
-        (recur (inc index) (rest remaining) "" nil (if (is-valid-num current-num current-num-index 0 graph)
-                                                     (conj valid-nums (Integer/parseInt current-num))
-                                                     valid-nums))
-        :else
-        (recur (inc index) (rest remaining) current-num current-num-index valid-nums)))))
+  (apply + (map-indexed (fn [y row] (find-valid-nums graph row y)) graph)))
