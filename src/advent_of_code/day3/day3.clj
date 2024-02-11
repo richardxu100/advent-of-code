7f630010674060a5770b25acae9279bc40bfadf4
Richard Xu <richard.xu@roadbotics.com>
2024-02-10 23:51:01 -0500

    Make tests pass

--- src/advent_of_code/day3/day3.clj
+++ src/advent_of_code/day3/day3.clj
@@ -1,7 +1,24 @@
 (ns advent-of-code.day3.day3)
 
+(defn is-digit?
+  "Check if the given character represents a digit."
+  [char]
+  (try
+    (Integer/parseInt (str char))
+    true
+    (catch NumberFormatException _
+      false)))
+
+
 (defn sum-of-parts
   "docstring"
   [graph]
-  4)
-
+  (let [row (first graph)]
+    (loop [remaining row]
+      (cond
+        (empty? remaining)
+        0
+        (is-digit? (first remaining))
+        (Integer/parseInt (first remaining))
+        :else
+        (recur (rest remaining))))))
--- src/advent_of_code/day3/day3_test.clj
+++ src/advent_of_code/day3/day3_test.clj
@@ -3,7 +3,7 @@
             [advent-of-code.day3.day3 :as day3]))
 
 (deftest it-returns-for-single-valid-number
-  (is (= 4 (day3/sum-of-parts [["*4"]]))))
+  (is (= 4 (day3/sum-of-parts [["*" "4"]]))))
 
 (deftest it-returns-for-other-single-valid-number
-  (is (= 3 (day3/sum-of-parts [["*3"]]))))
+  (is (= 3 (day3/sum-of-parts [["*" "3"]]))))
