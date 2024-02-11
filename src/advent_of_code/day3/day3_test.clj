72ef404cab8b421af119d0d842ecf1af3b193c92
Richard Xu <richard.xu@roadbotics.com>
2024-02-10 23:36:49 -0500

    Add failing it-returns-for-other-single-valid-number

--- src/advent_of_code/day3/day3_test.clj
+++ src/advent_of_code/day3/day3_test.clj
@@ -4,3 +4,6 @@
 
 (deftest it-returns-for-single-valid-number
   (is (= 4 (day3/sum-of-parts [["*4"]]))))
+
+(deftest it-returns-for-other-single-valid-number
+  (is (= 3 (day3/sum-of-parts [["*3"]]))))
