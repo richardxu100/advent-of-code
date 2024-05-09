461e3a207c3ef636c02fe16eb6f2975a71f08ced
Richard Xu <richard.xu@roadbotics.com>
2024-05-09 15:59:18 -0400

    Make test pass

--- src/advent_of_code/day6/day6.clj
+++ src/advent_of_code/day6/day6.clj
@@ -1,6 +1,13 @@
 (ns advent-of-code.day6.day6)
 
+(defn calc-distance [hold-time total-time]
+  (* hold-time (- total-time hold-time)))
+
 (defn margin-of-error
   "docstring"
   [races]
-  nil)
+  (let [only-race (first races)
+        total-time (:time only-race)
+        record (:record only-race)]
+    (count (for [hold-time (range total-time) :when (> (calc-distance hold-time total-time) record)]
+       hold-time))))
