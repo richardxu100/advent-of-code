e2b1ea0fad6e0bb6acc2de23d6a590defaf860c1
Richard Xu <richardxu100@gmail.com>
2026-01-18 20:23:07 -0500

    Solve sample input day1 2025

---
+++ src/advent_of_code_2025/day1/day1.clj
@@ -0,0 +1,62 @@
+(ns advent-of-code-2025.day1.day1
+  (:require [clojure.string :as str]))
+
+(def sample-input "./src/advent_of_code_2025/day1/sample_input.txt")
+
+(defn parse-instruction [s]
+  {:dir (if (= \L (first s)) :left :right)
+   :rotation (parse-long (apply str (rest s)))})
+
+(parse-instruction "L12")
+(parse-instruction "R1")
+
+
+(first "l12")
+(apply str (rest "l1"))
+
+(defn parse-instructions [input]
+  (->> input
+       slurp
+       str/split-lines
+       (map parse-instruction)))
+
+(def sample-instructions (parse-instructions sample-input))
+
+(defn add-until-in-range [pos]
+  (loop [new-pos pos]
+    (if (< new-pos 0)
+      (recur (+ new-pos 100))
+      new-pos)))
+
+(defn subtract-until-in-range [pos]
+  (loop [new-pos pos]
+    (if (> new-pos 99)
+      (recur (- new-pos 100))
+      new-pos)))
+
+(add-until-in-range -131)
+(subtract-until-in-range 332)
+
+(defn apply-instruction [pos {dir :dir rotation :rotation}]
+  (let [new-pos (if (= dir :left) (- pos rotation) (+ pos rotation))]
+    (cond
+      (< new-pos 0)
+      (add-until-in-range new-pos)
+      (> new-pos 99)
+      (subtract-until-in-range new-pos)
+      :else
+      new-pos)))
+
+(reduce apply-instruction 50 sample-instructions)
+
+(apply-instruction 50 (first (parse-instructions sample-input)))
+
+(defn gen-position-history [pos instructions]
+  (loop [history [pos]
+         remaining-instructions instructions]
+    (if (empty? remaining-instructions)
+      history
+      (recur (conj history (apply-instruction (last history) (first remaining-instructions)))
+             (rest remaining-instructions)))))
+
+(gen-position-history 50 sample-instructions)
