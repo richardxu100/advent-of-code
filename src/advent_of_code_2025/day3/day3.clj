259fe5e903ee2f04deb26f1ba7a4897db387957a
Richard Xu <richardxu100@gmail.com>
2026-01-19 23:00:17 -0500

    Cleanup

--- src/advent_of_code_2025/day3/day3.clj
+++ src/advent_of_code_2025/day3/day3.clj
@@ -4,15 +4,9 @@
 (def sample-input "./src/advent_of_code_2025/day3/sample_input.txt")
 (def real-input "./src/advent_of_code_2025/day3/input.txt")
 
-
-(defn max-voltage [s]
-  (let [nums (map (comp parse-long str) s)
-        left (apply max (drop-last 1 nums))
-        left-index (.indexOf s (str left))
-        right (apply max (drop (inc left-index) nums))]
-    (parse-long (str left right))))
-
-(defn part2-max-voltage [s length]
+(defn max-voltage
+  "Find the max number in string s with length"
+  [length s]
   (let [nums (map (comp parse-long str) s)]
     (loop [current-volts []
            remaining-nums nums]
@@ -23,33 +17,22 @@
               max-volt-index (.indexOf remaining-nums max-volt)]
           (recur (conj current-volts max-volt) (drop (inc max-volt-index) remaining-nums)))))))
 
-(comment
-  (part2-max-voltage "13746892" 4)
-  (part2-max-voltage "987654321111111" 12)
-  (part2-max-voltage "234234234234278" 12))
-
-(comment
-  (.indexOf "8292" "9")
-  (max-voltage "8292")
-  (map parse-long "1232"))
-
 (defn part1 [input]
   (->> input
        slurp
        str/split-lines
-       (map max-voltage)
+       (map (partial max-voltage 2))
        (reduce +)))
 
 (comment
   (part1 sample-input)
   (part1 real-input))
 
-
 (defn part2 [input]
   (->> input
        slurp
        str/split-lines
-       (map (fn [s] (part2-max-voltage s 12)))
+       (map (partial max-voltage 12))
        (reduce +)))
 
 (comment
