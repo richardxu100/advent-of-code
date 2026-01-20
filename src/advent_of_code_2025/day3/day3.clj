7a437d01833fcfd9f106507988ff1d5fd75f919c
Richard Xu <richardxu100@gmail.com>
2026-01-19 22:46:29 -0500

    Finish part2 day3

--- src/advent_of_code_2025/day3/day3.clj
+++ src/advent_of_code_2025/day3/day3.clj
@@ -12,6 +12,21 @@
         right (apply max (drop (inc left-index) nums))]
     (parse-long (str left right))))
 
+(defn part2-max-voltage [s length]
+  (let [nums (map (comp parse-long str) s)]
+    (loop [current-volts []
+           remaining-nums nums]
+      (if (= (count current-volts) length)
+        (parse-long (apply str current-volts))
+        (let [max-volt (apply max (drop-last (- length 1 (count current-volts)) remaining-nums))
+              max-volt-index (.indexOf remaining-nums max-volt)]
+          (recur (conj current-volts max-volt) (drop (inc max-volt-index) remaining-nums) ))))))
+
+(part2-max-voltage "13746892" 4)
+(part2-max-voltage "987654321111111" 12)
+(part2-max-voltage "234234234234278" 12)
+
+
 (comment
   (.indexOf "8292" "9")
   (max-voltage "8292")
@@ -27,3 +42,15 @@
 (comment
   (part1 sample-input)
   (part1 real-input))
+
+
+(defn part2 [input]
+  (->> input
+       slurp
+       str/split-lines
+       (map (fn [s] (part2-max-voltage s 12)))
+       (reduce +)))
+
+(part2 sample-input)
+(part2 real-input)
+
