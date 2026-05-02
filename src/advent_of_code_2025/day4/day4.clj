9b2689884171d39233ae153800aeaef8eda5dce7
Richard Xu <richardxu100@gmail.com>
2026-05-02 14:41:34 -0400

    Cleanup

--- src/advent_of_code_2025/day4/day4.clj
+++ src/advent_of_code_2025/day4/day4.clj
@@ -49,45 +49,41 @@
   (map #(g/get-val test-graph %) (grab-neighbors test-graph [1 3]))
   (part1 test-graph))
 
-(part1 (g/parse-graph real-input))
+(part1 real-graph)
 
-(def simple-graph [["@" "*"]
-                   ["*" "@"]])
+(comment
+  (def simple-graph [["@" "*"]
+                     ["*" "@"]])
+  (assoc-in simple-graph [0 0] "o"))
 
-(assoc-in simple-graph [0 0] "o")
 (defn remove-toilet-paper [graph points]
   (loop [updated-graph graph
          remaining-points points]
     (if (empty? remaining-points)
       updated-graph
+      ; need to reverse the point to update the graph properly
       (recur (assoc-in updated-graph (reverse (first remaining-points)) ".") (rest remaining-points)))))
 
-(reverse [0 1])
-test-graph
-(assoc-in test-graph [0 1] ".")
-(assoc-in test-graph [2 0] ".")
+(comment
+  (reverse [0 1])
+  test-graph
+  (assoc-in test-graph [0 1] ".")
+  (assoc-in test-graph [2 0] "."))
 
 (comment
   (remove-toilet-paper simple-graph [[0 0] [1 1]]))
 
-;(defn part2 [graph]
-;  (let [removable-indices
-;        (->> (grab-indices graph)
-;             (filter #(and (toilet-paper? (g/get-val graph %))
-;                           (accessible-by-forklift? graph %))))]
-;    (remove-toilet-paper graph removable-indices)))
-
 (defn part2 [graph]
   (loop [updated-graph graph
          forkliftable-indices (find-forkliftable-indices graph)
-         forklifted-count 0]
+         removed-count 0]
     (if (empty? forkliftable-indices)
-      forklifted-count
-      (recur (remove-toilet-paper updated-graph forkliftable-indices) (find-forkliftable-indices (remove-toilet-paper updated-graph forkliftable-indices)) (+ forklifted-count (count forkliftable-indices))))))
+      removed-count
+      (recur (remove-toilet-paper updated-graph forkliftable-indices) (find-forkliftable-indices (remove-toilet-paper updated-graph forkliftable-indices)) (+ removed-count (count forkliftable-indices))))))
 
-(part2 simple-graph)
-(part2 test-graph)
-(part2 real-graph)
-(remove-toilet-paper test-graph [[2 0]])
-
-(remove-toilet-paper test-graph [[0 1]])
+(comment
+  (part2 simple-graph)
+  (part2 test-graph)
+  (part2 real-graph)
+  (remove-toilet-paper test-graph [[2 0]])
+  (remove-toilet-paper test-graph [[0 1]]))
