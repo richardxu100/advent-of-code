60cdd317e49d5afa280bccc0354a2baa737acf54
Richard Xu <richardxu100@gmail.com>
2026-05-02 09:34:21 -0400

    Cleanup

--- src/advent_of_code_2025/day4/day4.clj
+++ src/advent_of_code_2025/day4/day4.clj
@@ -17,11 +17,11 @@
         neighbor-indices-in-graph (filter #(g/is-in-graph? graph %) neighbor-indices)]
     (> 4 (count (filter #(= "@" %) (map #(g/get-graph-value graph %) neighbor-indices-in-graph))))))
 
-(g/find-neighbor-indices [3 2])
-(valid-neighbors? test-graph [1 0])
-
-(valid-neighbors? test-graph [1 3])
-(grab-neighbors test-graph [1 3])
+(comment
+  (g/find-neighbor-indices [3 2])
+  (valid-neighbors? test-graph [1 0])
+  (valid-neighbors? test-graph [1 3])
+  (grab-neighbors test-graph [1 3]))
 (defn accessible-by-forklift? [graph [x y]]
   (and (= "@" (g/get-graph-value graph [x y]))
        (valid-neighbors? graph [x y])))
@@ -31,18 +31,16 @@
         y (range (count graph))]
     [x y]))
 
-
-(grab-indices test-graph)
-
-test-graph
+(comment
+  (grab-indices test-graph)
+  test-graph)
 (defn part1 [graph]
-  (filter #(accessible-by-forklift? graph %) (grab-indices graph)))
+  (count (filter #(accessible-by-forklift? graph %) (grab-indices graph))))
 
-(map #(g/get-graph-value test-graph %) (grab-neighbors test-graph [1 3]))
-
-(part1 test-graph)
+(comment
+  (map #(g/get-graph-value test-graph %) (grab-neighbors test-graph [1 3]))
+  (part1 test-graph))
 
 (part1 (g/parse-graph real-input))
-;; figure out the bug here!
 
 
