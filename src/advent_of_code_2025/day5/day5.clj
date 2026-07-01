a6cbe7a778135bfb5489d6a8a3a01243da45638d
richard <richardxu100@gmail.com>
2026-07-01 14:14:58 -0400

    Cleanup

--- src/advent_of_code_2025/day5/day5.clj
+++ src/advent_of_code_2025/day5/day5.clj
@@ -5,34 +5,6 @@
 (def test-input "./src/advent_of_code_2025/day5/test_input.txt")
 (def real-input "./src/advent_of_code_2025/day5/input.txt")
 
-(comment
-  (defn old-convert-to-range [s]
-    (let [[l r] (str/split s #"-")]
-      (range (parse-long l) (inc (parse-long r)))))
-
-  (old-convert-to-range "3-6")
-
-  (defn old-parse-input [input]
-    (let [lines (str/split-lines (slurp input))
-          split-index (.indexOf lines "")]
-      {:ranges (map old-convert-to-range (take split-index lines))
-       :ids    (map parse-long (take-last (dec (- (count lines) split-index)) lines))}))
-
-  (old-parse-input test-input)
-
-  (defn destructure-practice [{r :ranges i :ids}]
-    (println r i))
-
-  (destructure-practice (old-parse-input test-input))
-
-  (concat #{1 2 3} (range 5 12))
-
-  (defn naive-part1 [input]
-    (let [{ranges :ranges ids :ids} (old-parse-input input)
-          valid-id-set (reduce #(set/union %1 (set %2)) #{} ranges)]
-      (count (filter #(contains? valid-id-set %) ids))))
-
-  (naive-part1 test-input))
 
 (defn convert-to-range [s]
   (let [[l r] (str/split s #"-")]
@@ -69,24 +41,7 @@
   (overlaps? [8 23] [5 10])
   (overlaps? [10 23] [5 10]))
 
-(defn cannot-consolidate? [ranges]
-  (loop [current-range (first ranges)
-         remaining-ranges (rest ranges)
-         remaining-ranges' (rest ranges)]
-    (cond
-      (and (empty? remaining-ranges) (empty? remaining-ranges'))
-      true
-      (empty? remaining-ranges)
-      (recur (first remaining-ranges') (rest remaining-ranges') (rest remaining-ranges'))
-      (overlaps? current-range (first remaining-ranges))
-      false
-      :else
-      (recur current-range (rest remaining-ranges) remaining-ranges'))))
-
-(cannot-consolidate? [[1 3] [3 9]])
-(cannot-consolidate? [[1 2] [3 9]])
-
-(defn find-any-overlapping-range [range remaining-ranges]
+(defn find-overlapping-range [range remaining-ranges]
   (loop [remaining-ranges' remaining-ranges]
     (cond
       (empty? remaining-ranges')
@@ -103,14 +58,13 @@
         max-y (max (second r1) (second r2))]
     {:range [min-x max-y] :id (:id indexed-r1)}))
 
-(merge-range {:range [10 23]} {:range [16 29]})
 
 (defn remove-id [ranges id]
   (filter #(not (= (:id %) id)) ranges))
 
-(remove-id [{:id 5 :range [10 23]} {:id 12 :range [10 25]}] 5)
-
-(defn consolidate [indexed-ranges]
+(defn consolidate
+  "Merge ranges until there are no overlaps"
+  [indexed-ranges]
   (loop [current-range (first indexed-ranges)
          remaining-ranges (rest indexed-ranges)
          consolidated-ranges []]
@@ -120,37 +74,27 @@
         consolidated-ranges
         (cons current-range consolidated-ranges))
       :else
-      (let [overlapping-range (find-any-overlapping-range current-range remaining-ranges)]
+      (let [overlapping-range (find-overlapping-range current-range remaining-ranges)]
         (if (nil? overlapping-range)
           (recur (first remaining-ranges) (rest remaining-ranges) (cons current-range consolidated-ranges))
           (let [consolidated-range (merge-range current-range overlapping-range)
                 remaining-ranges' (remove-id remaining-ranges (:id overlapping-range))]
             (recur consolidated-range remaining-ranges' consolidated-ranges)))))))
 
-;; probably need to use vectors. Also use debugger mode. And figure out how to create functions, rename things, other lsp things
-
-
-(defn handle-add-range [non-overlapping-ranges range]
-  (loop [ranges (conj non-overlapping-ranges range)]
-    (if (cannot-consolidate? ranges)
-      ranges
-      (recur (consolidate ranges)))))
-
 
 (defn convert-to-indexed-range [index s]
   (let [[l r] (str/split s #"-")]
     {:id index :range [(parse-long l) (parse-long r)]}))
-
 
 (defn parse-ranges-part-2 [input]
   (let [lines (str/split-lines (slurp input))
         split-index (.indexOf lines "")]
     (map-indexed convert-to-indexed-range (take split-index lines))))
 
-(parse-ranges-part-2 test-input)
-
-(consolidate (parse-ranges-part-2 test-input))
-(consolidate (parse-ranges-part-2 real-input))
+(comment
+  (parse-ranges-part-2 test-input)
+  (consolidate (parse-ranges-part-2 test-input))
+  (consolidate (parse-ranges-part-2 real-input)))
 
 (defn get-range-size [{r :range}]
   (inc (- (second r) (first r))))
@@ -161,5 +105,4 @@
     (reduce + (map get-range-size non-overlapping-ranges))))
 
 (part2 test-input)
-
 (part2 real-input)
