bcd158cfd4b722b6f6f6e7b8afae87854771ce3a
Richard Xu <richardxu100@gmail.com>
2026-08-17 17:51:32 -0400

    Finish part1 day 7

--- src/advent_of_code_2025/day7/day7.clj
+++ src/advent_of_code_2025/day7/day7.clj
@@ -7,6 +7,7 @@
 ;; Each term, refresh the board
 
 (def test-input "./src/advent_of_code_2025/day7/test_input.txt")
+(def input "./src/advent_of_code_2025/day7/input.txt")
 
 (def test-graph
   (g/parse-graph test-input))
@@ -30,7 +31,8 @@
 
 (defn extend-beam-to-next-row [board [x y]]
   (if (splitter? (g/get-val board [x (inc y)]))
-    (add-beams board [[(dec x) (inc y)] [(inc x) (inc y)]])))
+    (add-beams board [[(dec x) (inc y)] [(inc x) (inc y)]])
+    (add-beams board [[x (inc y)]])))
 
 (defn update-next-row [board index]
   (let [current-row (nth board index)
@@ -43,15 +45,57 @@
 
 (map #(vector % 4) [3 1 5 2])
 
-(extend-beam-to-next-row (first-turn test-graph) [7 1])
+(extend-beam-to-next-row (extend-beam-to-next-row (first-turn test-graph) [7 1]) [6 2])
 
 (update-next-row (first-turn test-graph) 1)
 
 (defn update [board index]
-  (case index
+  (condp = index
     0 (first-turn board)
     (dec (count board)) board
-    :else (update-next-row board index)))
+    (update-next-row board index)))
+
+(defn game [input]
+  (let [board (g/parse-graph input)]
+    (loop [board' board
+           turn 0]
+      (if (= turn (dec (count board)))
+        board'
+        (recur (update board' turn) (inc turn))))))
+
+(game test-input)
+
+(defn surrounded-by-beams? [row index]
+  (and (beam? (nth row (dec index))) (beam? (nth row (inc index)))))
+
+
+(defn num-splits [idx board]
+  (let [splitter-indices (->> (nth board idx)
+                              (map-indexed vector)
+                              (filter #(splitter? (second %)))
+                              (map first))]
+    (->> splitter-indices
+         (filter #(beam? (g/get-val board [% (dec idx)]))) ; not sure this is correct
+         count)))
+
+(defn calc-num-splits [board]
+  (reduce + (for [idx (range (count board))]
+              (num-splits idx board))))
 
 (defn part1 [input]
-  )
+  (let [result (game input)]
+    (calc-num-splits result)))
+
+(part1 test-input)
+
+;; run this in debug mode. There's a small bug somewhere
+
+(nth test-graph 0)
+
+(num-splits 15 test-graph)
+
+(count test-graph)
+
+(count (game test-input))
+
+(part1 input)
