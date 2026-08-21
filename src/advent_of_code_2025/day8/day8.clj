12b4b6beba2fcd0b9c18786e17f8e5808f4fa857
Richard Xu <richardxu100@gmail.com>
2026-08-21 17:55:18 -0400

    Get the connection code to kind of work
    It's wrong, but there are no runtime errors currently

--- src/advent_of_code_2025/day8/day8.clj
+++ src/advent_of_code_2025/day8/day8.clj
@@ -57,75 +57,77 @@
 
 (parse-nodes "[592 479 940],[862 61 35]")
 
-(defn build-graph [[n1 n2]]
+(defn build-circuit [[n1 n2]]
   (hash-map (str (random-uuid))
             {:connections {n1 #{n2} , n2 #{n1}}})) ; I may not need the connections keyword, could flatten this
 
-(def test-graph
-  (build-graph ['(1 2 3) '(4 5 6)]))
+(def test-circuit
+  (build-circuit ['(1 2 3) '(4 5 6)]))
 
-(vals test-graph)
+; (vals test-circuit)
 
 ;; fix this
-(defn has-node? [graph n]
-  (->> (vals graph)
-       first
+(defn has-node? [circuit-entry n]
+  (->> (val circuit-entry)
        :connections
        keys
        (some #(= n %))
-       boolean
-       ))
-
-(has-node? test-graph [1 2 3])
+       boolean))
 
 ;; all graphs, in this case is not a collection, but a map, in this case
-(defn find-corresponding-graph [all-graphs n]
-    (->> all-graphs
-         
+(defn find-corresponding-circuit-id [circuits n]
+  #dbg
+  (->> circuits
        (filter #(has-node? % n))        ;      first
-       first))
+       first
+       first
+       ))
 
-; I'm going to use a record class. The models are getting too confusing
 
-(find-corresponding-graph test-graph '(1 2 3))
+; A map entry looks like a two element array of the key and value
+; That makes sense
+(find-corresponding-circuit-id test-circuit '(1 2 3))
 
-(defn find-graphs-for-nodes [all-graphs [n1 n2]]
-    (let [n1-graph (find-corresponding-graph all-graphs n1)
-        n2-graph (find-corresponding-graph all-graphs n2)]
-    {n1 n1-graph, n2 n2-graph}))
+(defn find-circuit-ids-for-nodes [circuits [n1 n2]]
+  (let [n1-circuit-id (find-corresponding-circuit-id circuits n1)
+        n2-circuit-id (find-corresponding-circuit-id circuits n2)]
+    {n1 n1-circuit-id, n2 n2-circuit-id}))
 
 (defn update-connections [connections [n1 n2]]
   (merge connections {n1 (conj (get connections n1 []) n2)
                       n2 (conj (get connections n2 []) n1)}))
 
 ; kind of works, make sure everything is clojure list
-(update-connections (:connections (first (vals test-graph))) [[3 4 5] [4 5 6]])
+(update-connections (:connections (first (vals test-circuit))) [[3 4 5] [4 5 6]])
 
-(defn add-to-graph [all-graphs graphs-for-nodes]
-  (let [[n1 n2] (keys graphs-for-nodes)]
-    (if (nil? (get graphs-for-nodes n1))
-      (update-in all-graphs [n2 :connections] #(update-connections % [n1 n2]))
-      (update-in all-graphs [n1 :connections] #(update-connections % [n1 n2])))))
+(defn add-to-circuit [circuits circuit-ids-for-nodes]
+  (let [[n1 n2] (keys circuit-ids-for-nodes)
+        n1-circuit-id (get circuit-ids-for-nodes n1)
+        n2-circuit-id (get circuit-ids-for-nodes n2)]
+    (if (nil? n1-circuit-id)
+      (update-in circuits [n2-circuit-id :connections] #(update-connections % [n1 n2]))
+      (update-in circuits [n1-circuit-id :connections] #(update-connections % [n1 n2])))))
 
-(defn merge-graphs [all-graphs graphs-for-nodes]
+(defn merge-circuits [all-circuits graphs-for-nodes]
+  all-circuits
   ) ;; not doing anything yet
 
-(defn- calc-num-existing-connections [graphs-for-nodes]
-  (count (filter seq (vals graphs-for-nodes))))
+(defn- calc-num-existing-circuits [circuit-ids-for-nodes]
+  (count (filter seq (vals circuit-ids-for-nodes))))
 
-(defn update-graphs [all-graphs graphs-for-nodes]
-  (condp = (calc-num-existing-connections graphs-for-nodes)
-    0 (conj all-graphs (build-graph (keys graphs-for-nodes)))
-    1 (add-to-graph all-graphs graphs-for-nodes)
-    2 (merge-graphs all-graphs graphs-for-nodes)))
+(defn update-graphs [circuits circuit-ids-for-nodes]
+  (condp = (calc-num-existing-circuits circuit-ids-for-nodes)
+    0 (conj circuits (build-circuit (keys circuit-ids-for-nodes)))
+    1 (add-to-circuit circuits circuit-ids-for-nodes)
+    2 (merge-circuits circuits circuit-ids-for-nodes)))
 
-(defn build-graphs [all-graphs [route _]]
+(defn build-circuits [circuits [route _]]
   (let [nodes (parse-nodes route)
-        graphs-for-nodes (find-graphs-for-nodes all-graphs nodes)]
-    (update-graphs all-graphs graphs-for-nodes)))
+        circuit-ids-for-nodes (find-circuit-ids-for-nodes circuits nodes)]
+    (update-graphs circuits circuit-ids-for-nodes)))
 
 (defn connect-circuits [distance-map]
   (let [sorted-distance-map (sort-by val < distance-map)]
-    (reduce build-graphs {} sorted-distance-map)))
+    (reduce build-circuits {} sorted-distance-map)))
 
 (connect-circuits (gen-distance-map test-coords))
