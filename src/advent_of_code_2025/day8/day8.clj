4d79fe178de68e9d9901bd3e1e9e5d2ce4d25cd4
Richard Xu <richardxu100@gmail.com>
2026-08-22 15:43:54 -0400

    Cleanup

--- src/advent_of_code_2025/day8/day8.clj
+++ src/advent_of_code_2025/day8/day8.clj
@@ -42,24 +42,17 @@
        (sort-by val <)
        (take-nth 2))) ; last step removes the duplicate routes
 
-(gen-sorted-distance-map test-coords)
-
-(->
- (gen-distance-map test-coords)
- (get (coord-key (first test-coords) (second test-coords))))
-
-(take-nth 2 (sort-by val <
-                     (gen-distance-map (parse-coords test-input))))
-
-
-;; It looks like I'll iterate through the distance map, and connect, and see what graph is created
-
-;; Can use reduce to build the graph. To calc the segments, that's probably a graph traversal
-;; with some duplicated set. Maybe I'll need to memoize like the last problem
+(comment
+  (gen-sorted-distance-map test-coords)
+  (->
+   (gen-distance-map test-coords)
+   (get (coord-key (first test-coords) (second test-coords))))
+  (take-nth 2 (sort-by val <
+                       (gen-distance-map (parse-coords test-input)))))
 
 (defn parse-nodes [route]
   (->> (str/split route #",")
-       (map #(re-seq #"\d+" %)) 
+       (map #(re-seq #"\d+" %))
        (map #(map parse-long %))))
 
 (parse-nodes "[592 479 940],[862 61 35]")
@@ -87,7 +80,6 @@
        (filter #(has-node? % n))        ;      first
        first
        first))
-
 
 ; A map entry looks like a two element array of the key and value
 ; That makes sense
@@ -127,16 +119,18 @@
     (hash-map (str (random-uuid))
               {:connections updated-connections})))
 
-(def test-circuit-1 (build-circuit ['(1 2 3) '(4 5 6)]))
-(def test-circuit-2 (build-circuit ['(:a :b :c) '(:d :e :f)]))
+(comment
+  (def test-circuit-1 (build-circuit ['(1 2 3) '(4 5 6)]))
+  (def test-circuit-2 (build-circuit ['(:a :b :c) '(:d :e :f)]))
 
-(def test-circuit-1-id (first (keys test-circuit-1)))
-(def test-circuit-2-id (first (keys test-circuit-2)))
+  (def test-circuit-1-id (first (keys test-circuit-1)))
+  (def test-circuit-2-id (first (keys test-circuit-2)))
 
-(build-merged-circuit (merge test-circuit-1 test-circuit-2) {'(4 5 6) test-circuit-1-id
-                                                             '(:d :e :f) test-circuit-2-id})
+  (build-merged-circuit (merge test-circuit-1 test-circuit-2) {'(4 5 6) test-circuit-1-id
+                                                               '(:d :e :f) test-circuit-2-id}))
 
 ; not always needed, if in the same circuit... Then can just do nothing
+; Looks like it doesn't hurt, I should probably remove the connection though here. May hurt for part 2
 (defn merge-circuits [circuits circuit-ids-for-nodes]
   (let [merged-circuit (build-merged-circuit circuits circuit-ids-for-nodes)
         [circuit-id1 circuit-id2] (vals circuit-ids-for-nodes)]
@@ -151,7 +145,7 @@
 (defn- calc-num-existing-circuits [circuit-ids-for-nodes]
   (count (filter seq (vals circuit-ids-for-nodes))))
 
-(defn update-graphs [circuits circuit-ids-for-nodes]
+(defn update-circuits [circuits circuit-ids-for-nodes]
   (condp = (calc-num-existing-circuits circuit-ids-for-nodes)
     0 (conj circuits (build-circuit (keys circuit-ids-for-nodes)))
     1 (add-to-circuit circuits circuit-ids-for-nodes)
@@ -160,7 +154,7 @@
 (defn build-circuits [circuits [route _]]
   (let [nodes (parse-nodes route)
         circuit-ids-for-nodes (find-circuit-ids-for-nodes circuits nodes)]
-    (update-graphs circuits circuit-ids-for-nodes)))
+    (update-circuits circuits circuit-ids-for-nodes)))
 
 (defn connect-circuits [sorted-distance-map num-to-connect]
   (reduce build-circuits {} (take num-to-connect sorted-distance-map)))
@@ -168,54 +162,24 @@
 (defn- in-same-circuit? [circuit-ids-for-nodes]
   (= 1 (count (set (vals circuit-ids-for-nodes)))))
 
-(in-same-circuit? {'(1 2 3) "abc" '(4 5 6) "abcd"})
-
-(defn connect-junction-boxes [sorted-distance-map num-to-connect]
-  (loop [remaining-entries sorted-distance-map
-         remaining-connections num-to-connect
-         circuits {}]
-    (if (zero? remaining-connections)
-      circuits
-      (let [current-entry (first remaining-entries)
-            nodes (parse-nodes (first current-entry))
-            circuit-ids-for-nodes (find-circuit-ids-for-nodes circuits nodes)]
-        (condp = (calc-num-existing-circuits circuit-ids-for-nodes)
-          0 (recur (rest remaining-entries) (dec remaining-connections)
-                   (conj circuits (build-circuit (keys circuit-ids-for-nodes))))
-          1 (recur (rest remaining-entries) (dec remaining-connections)
-                   (add-to-circuit circuits circuit-ids-for-nodes))
-          2 (if (in-same-circuit? circuit-ids-for-nodes)
-              (recur (rest remaining-entries) (dec remaining-connections) circuits) ; i don't think i should dec here
-              (recur (rest remaining-entries) (dec remaining-connections)
-                     (merge-circuits circuits circuit-ids-for-nodes))))))))
-
-(connect-circuits (gen-sorted-distance-map test-coords) 4)
-
-(defn- parse-node-display [[_ {connections :connections}]]
-  (keys connections))
-
-(def test-sorted-distance-map (gen-sorted-distance-map test-coords))
-test-sorted-distance-map
-
-
-(map parse-node-display (connect-junction-boxes test-sorted-distance-map 10))
-
+(comment
+  (in-same-circuit? {'(1 2 3) "abc" '(4 5 6) "abcd"})
+  (connect-circuits (gen-sorted-distance-map test-coords) 4)
+  (def test-sorted-distance-map (gen-sorted-distance-map test-coords))
+  test-sorted-distance-map)
 
 (defn num-nodes-in-circuit [[_ {connections :connections}]]
   (count (keys connections)))
 
 
-(defn part1 [input]
+(defn part1 [input num-connections]
   (let [coords (parse-coords input)
         sorted-distance-map (gen-sorted-distance-map coords)
-        circuits (connect-junction-boxes sorted-distance-map 1000)
+        circuits (connect-circuits sorted-distance-map num-connections)
         three-largest-circuits (take 3 (sort-by num-nodes-in-circuit > circuits))]
-    (print three-largest-circuits)
     (reduce * (map num-nodes-in-circuit three-largest-circuits))))
 
-(part1 test-input)
-(part1 input)
-
-;; I still need to finish merge circuits
+(part1 test-input 10)
+(part1 input 1000)
 
 
