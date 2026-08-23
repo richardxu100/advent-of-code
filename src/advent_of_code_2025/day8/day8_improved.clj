338e04cf37afb42093077e3d98ccaa41c7df9f2d
Richard Xu <richardxu100@gmail.com>
2026-08-23 16:10:37 -0400

    Add day8 improved

---
+++ src/advent_of_code_2025/day8/day8_improved.clj
@@ -0,0 +1,77 @@
+(ns advent-of-code-2025.day8.day8-improved 
+  (:require
+    [clojure.string :as str]
+    [clojure.set :as set]))
+
+(defn sqrd [x]
+  (Math/pow x 2))
+
+(defn distance [[x1 y1 z1] [x2 y2 z2]]
+  (Math/sqrt
+   (+ (sqrd (- x1 x2)) (sqrd (- y1 y2)) (sqrd (- z1 z2)))))
+
+(def test-input "./src/advent_of_code_2025/day8/test_input.txt")
+(def input "./src/advent_of_code_2025/day8/input.txt")
+
+(defn parse-points [input]
+  (->> (slurp input)
+       (str/split-lines)
+       (map #(str/split % #","))
+       (map (partial mapv parse-long))))
+
+(parse-points test-input)
+
+(defn sorted-connections [points]
+  (sort-by first (for [p1 points ; it looks like sort automatically compares by first element
+                       p2 points
+                       :when (not= p1 p2)]
+                   [(distance p1 p2) p1 p2])))
+
+(take-nth 2 (sorted-connections (parse-points test-input)))
+
+(defn create-circuits [points]
+  (->> points
+       (map hash-set)
+       set))
+
+(def test-circuits
+  (->> test-input
+       parse-points
+       create-circuits))
+
+(defn- find-circuit [circuits point]
+  (first (filter #(contains? % point) circuits)))
+
+(find-circuit test-circuits [431 825 988])
+
+(defn- update-circuits [circuits [p1 p2]]
+  (let [circuit1 (find-circuit circuits p1)
+        circuit2 (find-circuit circuits p2)]
+    (if (= circuit1 circuit2)
+      circuits
+      (conj (disj circuits circuit1 circuit2) (set/union circuit1 circuit2)))))
+
+(defn build-final-circuits [points num-connections]
+  (let [circuits (create-circuits points)
+        connections-to-process (->> (sorted-connections points)
+                                    (take-nth 2)
+                                    (map rest)
+                                    (take num-connections))]
+    (reduce update-circuits circuits connections-to-process)))
+
+(defn part1 [input num-connections]
+  (let [points (parse-points input)
+        final-circuits (build-final-circuits points num-connections)]
+    (->> final-circuits
+         (map count)
+         (sort >)
+         (take 3)
+         (reduce *))))
+
+(part1 test-input 10)
+(part1 input 1000)
+
+; If calculating distances is already n^2, maybe making the connection
+; algorithm n^2 isn't that bad too. I was worried about that, but the premature
+; optimization wasn't worth it
+
